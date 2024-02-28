df5506b4c9ae33f378f774d5699cdac1e9a13750
mehmetaydemir <mehmetaydemir032@gmail.com>
2024-02-28 10:04:42 +0300

    first commit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,107 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ReusableMethods;
+
+import java.time.Duration;
+
+public class LoginTest {
+
+    @Test
+    public void n11LoginTest(){
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+
+        driver.get("https://www.n11.com/");
+        ReusableMethods.bekle(3);
+
+        WebElement girisYap = driver.findElement(By.xpath("//*[@title='Giriş Yap']"));
+
+        //giris yap butonuna tıklanır
+        girisYap.click();
+        ReusableMethods.bekle(2);
+        WebElement n11Firsat = driver.findElement(By.xpath("//*[@class='dn-slide-deny-btn']"));
+        n11Firsat.click();
+        ReusableMethods.bekle(3);
+
+        //ePosta Kontrolleri
+        WebElement ePosta = driver.findElement(By.id("email"));
+
+        // ePosta boş olduğunda
+        ePosta.sendKeys(" ");
+        WebElement sifre = driver.findElement(By.id("password"));
+        sifre.click();
+        String expectedEPostaUyari = "Geçerli bir e-posta adresi girmelisin.";
+        //WebElement ePostaUyariBildirimi = driver.findElement(By.xpath("//*[@class='errorMessage']"));
+        //String actualEPostaUyari = ePostaUyariBildirimi.getText();
+        //Assert.assertEquals(expectedEPostaUyari,actualEPostaUyari);
+
+        // ePosta @ karakteri olmadığında
+        ePosta.clear();
+        ReusableMethods.bekle(2);
+        ePosta.sendKeys("mhmtaydemir32gmail.com");
+        sifre.click();
+        expectedEPostaUyari = "Geçerli bir e-posta adresi girmelisin.";
+        //actualEPostaUyari = ePostaUyariBildirimi.getText();
+        //Assert.assertEquals(expectedEPostaUyari,actualEPostaUyari);
+
+        // ePosta .com  olmadığında
+        ePosta.clear();
+        ReusableMethods.bekle(2);
+        ePosta.sendKeys("mhmtaydemir32@gmail");
+        sifre.click();
+        expectedEPostaUyari = "Geçerli bir e-posta adresi girmelisin.";
+        //actualEPostaUyari = ePostaUyariBildirimi.getText();
+        //Assert.assertEquals(expectedEPostaUyari,actualEPostaUyari);
+
+        //Şifre Kontrolleri
+        // en az 6 karakter kontrolü
+        ePosta.clear();
+        ReusableMethods.bekle(2);
+        ePosta.sendKeys("mhmtaydemir32@gmail.com");
+        sifre.sendKeys("1");
+        WebElement girisYapBtn = driver.findElement(By.id("loginButton"));
+        girisYapBtn.click();
+        ReusableMethods.bekle(5);
+        WebElement sifreUyariBildirimi = driver.findElement(By.xpath("(//*[@class='errorMessage'])[2]"));
+        String expectedSifreUyariEnAz = "Girilen değer en az 6 karakter olmalıdır.";
+        String actualSifreUyariEnAz = sifreUyariBildirimi.getText();
+        Assert.assertEquals(expectedSifreUyariEnAz,actualSifreUyariEnAz);
+
+        // en çok 15 karakter kontrolü
+        sifre.clear();
+        ReusableMethods.bekle(5);
+        sifre.sendKeys("ma12345678901234");
+        girisYapBtn.click();
+        ReusableMethods.bekle(5);
+        sifreUyariBildirimi = driver.findElement(By.xpath("(//*[@class='errorMessage'])[2]"));
+        String expectedSifreUyariEnCok = "Girilen değer en fazla 15 karakter olmalıdır.";
+        String actualSifreUyariEnCok = sifreUyariBildirimi.getText();
+        Assert.assertEquals(expectedSifreUyariEnCok,actualSifreUyariEnCok);
+
+
+
+
+
+        ReusableMethods.bekle(2);
+        driver.quit();
+
+
+
+
+
+
+    }
+
+
+
+}
